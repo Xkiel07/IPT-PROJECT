9358f9bab09dc7c0e1d629a10fc1accab53f44a0
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-18 12:29:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    resolve:{
-        alias:{
-            '@': '/resources',
-            '$': 'jquery'
-        }
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+            },
+        },
     }
-});
+})
